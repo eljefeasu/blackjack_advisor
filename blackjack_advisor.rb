d9fb731d2b39daea465b05c6f917c8b540abd403
Jefferson Neel <jefferson.neel@gmail.com>
2016-02-03 16:15:42 -0500

    Technically a working normal mode

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -13,7 +13,7 @@ def hard
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   hard_hash["9"] = {"2" => "Double if possible, otherwise hit",
                     "3" => "Double if possible, otherwise hit",
                     "4" => "Double if possible, otherwise hit",
@@ -23,7 +23,7 @@ def hard
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   hard_hash["10"] = {"2" => "Double if possible, otherwise hit",
                     "3" => "Double if possible, otherwise hit",
                     "4" => "Double if possible, otherwise hit",
@@ -33,7 +33,7 @@ def hard
                     "8" => "Double if possible, otherwise hit",
                     "9" => "Double if possible, otherwise hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   hard_hash["11"] = {"2" => "Double if possible, otherwise hit",
                     "3" => "Double if possible, otherwise hit",
                     "4" => "Double if possible, otherwise hit",
@@ -43,7 +43,7 @@ def hard
                     "8" => "Double if possible, otherwise hit",
                     "9" => "Double if possible, otherwise hit",
                     "10" => "Double if possible, otherwise hit",
-                    "A" => "Double if possible, otherwise hit"}
+                    "11" => "Double if possible, otherwise hit"}
   hard_hash["12"] = {"2" => "Hit",
                     "3" => "Hit",
                     "4" => "Stand",
@@ -53,7 +53,7 @@ def hard
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   hard_hash["13"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -63,7 +63,7 @@ def hard
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   hard_hash["14"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -73,7 +73,7 @@ def hard
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   hard_hash["15"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -83,7 +83,7 @@ def hard
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   hard_hash["16"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -93,7 +93,7 @@ def hard
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   hard_hash["17"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -103,7 +103,7 @@ def hard
                     "8" => "Stand",
                     "9" => "Stand",
                     "10" => "Stand",
-                    "A" => "Stand"}
+                    "11" => "Stand"}
   hard_hash["18"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -113,7 +113,7 @@ def hard
                     "8" => "Stand",
                     "9" => "Stand",
                     "10" => "Stand",
-                    "A" => "Stand"}
+                    "11" => "Stand"}
   hard_hash["19"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -123,7 +123,7 @@ def hard
                     "8" => "Stand",
                     "9" => "Stand",
                     "10" => "Stand",
-                    "A" => "Stand"}
+                    "11" => "Stand"}
   hard_hash["20"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -133,7 +133,7 @@ def hard
                     "8" => "Stand",
                     "9" => "Stand",
                     "10" => "Stand",
-                    "A" => "Stand"}
+                    "11" => "Stand"}
   hard_hash["21"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -143,7 +143,7 @@ def hard
                     "8" => "Stand",
                     "9" => "Stand",
                     "10" => "Stand",
-                    "A" => "Stand"}
+                    "11" => "Stand"}
 
   return hard_hash
 end
@@ -160,7 +160,7 @@ def soft
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   soft_hash["14"] = {"2" => "Hit",
                     "3" => "Hit",
                     "4" => "Double if possible, otherwise hit",
@@ -170,7 +170,7 @@ def soft
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   soft_hash["15"] = {"2" => "Hit",
                     "3" => "Hit",
                     "4" => "Double if possible, otherwise hit",
@@ -180,7 +180,7 @@ def soft
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   soft_hash["16"] = {"2" => "Hit",
                     "3" => "Hit",
                     "4" => "Double if possible, otherwise hit",
@@ -190,7 +190,7 @@ def soft
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   soft_hash["17"] = {"2" => "Double if possible, otherwise hit",
                     "3" => "Double if possible, otherwise hit",
                     "4" => "Double if possible, otherwise hit",
@@ -200,7 +200,7 @@ def soft
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   soft_hash["18"] = {"2" => "Stand",
                     "3" => "Double if possible, otherwise stand",
                     "4" => "Double if possible, otherwise stand",
@@ -210,7 +210,7 @@ def soft
                     "8" => "Stand",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Stand"}
+                    "11" => "Stand"}
   soft_hash["19"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -220,7 +220,7 @@ def soft
                     "8" => "Stand",
                     "9" => "Stand",
                     "10" => "Stand",
-                    "A" => "Stand"}
+                    "11" => "Stand"}
   soft_hash["20"]
   soft_hash["21"]
 
@@ -239,7 +239,7 @@ def pair
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   pair_hash["6"] = {"2" => "Split",
                     "3" => "Split",
                     "4" => "Split",
@@ -249,7 +249,7 @@ def pair
                     "8" => "Split",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   pair_hash["8"] = {"2" => "Hit",
                     "3" => "Hit",
                     "4" => "Split",
@@ -259,7 +259,7 @@ def pair
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   pair_hash["10"] = {"2" => "Double if possible, otherwise hit",
                     "3" => "Double if possible, otherwise hit",
                     "4" => "Double if possible, otherwise hit",
@@ -269,7 +269,7 @@ def pair
                     "8" => "Double if possible, otherwise hit",
                     "9" => "Double if possible, otherwise hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   pair_hash["12"] = {"2" => "Split",
                     "3" => "Split",
                     "4" => "Split",
@@ -279,7 +279,7 @@ def pair
                     "8" => "Hit",
                     "9" => "Hit",
                     "10" => "Hit",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   pair_hash["14"] = {"2" => "Split",
                     "3" => "Split",
                     "4" => "Split",
@@ -289,7 +289,7 @@ def pair
                     "8" => "Split",
                     "9" => "Hit",
                     "10" => "",
-                    "A" => "Hit"}
+                    "11" => "Hit"}
   pair_hash["16"]
   pair_hash["18"] = {"2" => "Split",
                     "3" => "Split",
@@ -300,7 +300,7 @@ def pair
                     "8" => "Split",
                     "9" => "Split",
                     "10" => "",
-                    "A" => ""}
+                    "11" => ""}
   pair_hash["20"] = {"2" => "Stand",
                     "3" => "Stand",
                     "4" => "Stand",
@@ -310,36 +310,53 @@ def pair
                     "8" => "Stand",
                     "9" => "Stand",
                     "10" => "Stand",
-                    "A" => "Stand"}
+                    "11" => "Stand"}
   pair_hash["22"]
 
 end
 
 def convert_card_to_number(card)
+
   card.upcase!
   if card == "J" || card == "Q" || card == "K"
     card = "10"
   elsif card == "A"
     card = "11"
   else
-    return
+    return card
   end
 end
 
-
 print "Please enter your first card: "
 first_card = gets.chomp
-convert_card_to_number(first_card)
 
 print "Pleae enter your second card: "
 second_card = gets.chomp
-convert_card_to_number(second_card)
+second_card = convert_card_to_number(second_card)
 
 print "Please enter dealer's card: "
 dealer_card = gets.chomp
+dealer_card = convert_card_to_number(dealer_card)
 
-player_card_total = first_card.to_i + second_card.to_i
-
-hard_hash = hard
-
-puts hard_hash[player_card_total.to_s][dealer_card.to_s]
+if first_card == 11 || second_card == 11
+  first_card = convert_card_to_number(first_card)
+  second_card = convert_card_to_number(second_card)
+  dealer_card = convert_card_to_number(dealer_card)
+  player_card_total = first_card.to_i + second_card.to_i
+  soft_hash = soft
+  puts soft_hash[player_card_total.to_s][dealer_card.to_s]
+elsif first_card == second_card
+  first_card = convert_card_to_number(first_card)
+  second_card = convert_card_to_number(second_card)
+  dealer_card = convert_card_to_number(dealer_card)
+  player_card_total = first_card.to_i + second_card.to_i
+  pair_hash = pair
+  puts pair_hash[player_card_total.to_s][dealer_card.to_s]
+else
+  first_card = convert_card_to_number(first_card)
+  second_card = convert_card_to_number(second_card)
+  dealer_card = convert_card_to_number(dealer_card)
+  player_card_total = first_card.to_i + second_card.to_i
+  hard_hash = hard
+  puts hard_hash[player_card_total.to_s][dealer_card.to_s]
+end
